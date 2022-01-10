9d92b2e5e182b8b0c9f51d3f9548e9ef8dc53d81
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2022-01-10 19:51:36 +0900

    Update install kucoin proxy.sh

--- install kucoin proxy.sh
+++ install kucoin proxy.sh
@@ -3,13 +3,8 @@ cd proxy
 git clone https://github.com/mikekonan/exchange-proxy.git
 cd exchange-proxy
 
-sudo apt-get purge golang*
-sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get -y purge golang*
+sudo add-apt-repository -y ppa:longsleep/golang-backports
 sudo apt update
-sudo apt install -y golang-go
-sudo apt install -y golang-easyjson
-
+sudo apt install -y golang-go golang-easyjson
 sudo make build
-
-Follow instructions for freqtrade config:
-https://github.com/mikekonan/exchange-proxy/blob/main/docs/ops/freqtrade.md 
