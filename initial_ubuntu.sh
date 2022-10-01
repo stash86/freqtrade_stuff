9304d34dff4d9c2f40ff39e9e0a4ab9f34de50e4
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2022-10-01 15:57:17 +0900

    update initial_ubuntu

--- initial_ubuntu.sh
+++ initial_ubuntu.sh
@@ -3,12 +3,14 @@ sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose python3-pip
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose python3-pip tailscale
 
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
 
 # # Docker without sudo
 # sudo groupadd docker
@@ -21,5 +23,6 @@ sudo chmod +x /usr/local/bin/docker-compose
 # sudo chown -R ubuntu:ubuntu ./freqtrade
 git clone https://github.com/stash86/freqtrade
 sudo chmod -R 0777 ./freqtrade
+cd freqtrade
 # sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/initial_freqtrade.sh
 # bash initial_freqtrade.sh
